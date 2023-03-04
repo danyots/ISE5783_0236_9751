b836eaf7e4ac0bddc89b59ed9c14316e4a2b56e8
dants <danieltsirkin2@gmail.com>
2023-03-05 01:51:33 +0200

    first commit

--- src/geometries/RadialGeometry.java
+++ src/geometries/RadialGeometry.java
@@ -1,9 +1,17 @@
 package geometries;
 
+import primitives.Point;
+import primitives.Vector;
+
 public abstract class RadialGeometry  implements Geometry{
     final protected double radius;
 
     public RadialGeometry(double r){
         radius=r;
+    }
+
+    @Override
+    public Vector getNormal(Point point) {
+        return null;
     }
 }
