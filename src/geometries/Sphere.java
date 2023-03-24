8d98ab06ce96584f59bdf7825ea20cd34fdc0c97
dants <danieltsirkin2@gmail.com>
2023-03-24 07:15:52 +0300

    first commit

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -3,17 +3,33 @@ package geometries;
 import primitives.Point;
 import primitives.Vector;
 
-public class Sphere extends RadialGeometry{
+/**
+ * The `Sphere` class represents a three-dimensional sphere in space, which is defined by a center point and a radius.
+ * It extends the `RadialGeometry` class and inherits its properties.
+ */
+public class Sphere extends RadialGeometry {
     final private Point center;
 
-    public Sphere(Point point, double r){
+    /**
+     * Constructs a new `Sphere` object with the specified center point and radius.
+     *
+     * @param center the center point of the sphere.
+     * @param r the radius of the sphere.
+     */
+    public Sphere(Point center, double r) {
         super(r);
-        center=point;
+        this.center = center;
     }
 
+    /**
+     * Returns the center point of the sphere.
+     *
+     * @return the center point of the sphere.
+     */
     public Point getCenter() {
         return center;
     }
+
 
     @Override
     public String toString() {
@@ -22,6 +38,7 @@ public class Sphere extends RadialGeometry{
                 ", radius=" + radius +
                 '}';
     }
+
 
     @Override
     public Vector getNormal(Point point) {
