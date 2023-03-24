65dc8c9b0b7881cc71d3bf1094f9d5b71dd9c169
dants <danieltsirkin2@gmail.com>
2023-03-24 07:03:30 +0300

    first commit

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -5,9 +5,21 @@ import java.util.List;
 import primitives.Point;
 import primitives.Vector;
 
-public class Triangle extends Polygon{
-    public Triangle(Point p1, Point p2, Point p3){
-        super(p1,p2,p3);
+/**
+ * The Triangle class is a polygon with three vertices.
+ * It extends the Polygon class and inherits its properties.
+ */
+public class Triangle extends Polygon {
+
+    /**
+     * Constructs a Triangle object with three vertices.
+     *
+     * @param p1 the first vertex of the triangle.
+     * @param p2 the second vertex of the triangle.
+     * @param p3 the third vertex of the triangle.
+     */
+    public Triangle(Point p1, Point p2, Point p3) {
+        super(p1, p2, p3);
     }
 
     @Override
@@ -18,3 +30,4 @@ public class Triangle extends Polygon{
                 '}';
     }
 }
+
