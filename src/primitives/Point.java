170320befb28f1091bf41c30822a4adbdf6a9ec1
dants <danieltsirkin2@gmail.com>
2023-03-24 06:13:24 +0300

    first commit

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -2,30 +2,74 @@ package primitives;
 
 import java.util.Objects;
 
+/**
+ * The Point class represents a point in 3D space.
+ */
 public class Point {
+    /**
+     * The 3D coordinates of this Point object.
+     */
     final Double3 xyz;
 
+    /**
+     * Constructs a new Point object with the specified coordinates.
+     *
+     * @param d1 the x-coordinate of the point
+     * @param d2 the y-coordinate of the point
+     * @param d3 the z-coordinate of the point
+     */
     public Point(double d1, double d2, double d3) {
         xyz = new Double3(d1, d2, d3);
     }
 
+    /**
+     * Constructs a new Point object from a Double3 object.
+     *
+     * @param doub3 the Double3 object to construct the Point from
+     */
     Point(Double3 doub3) {
         xyz = new Double3(doub3.d1, doub3.d2, doub3.d3);
 
     }
 
+    /**
+     * Returns a new Vector object that represents the difference between this point and another point.
+     * Uses the subtract function in Double3
+     *
+     * @param p the other point to subtract from this point
+     * @return a new Vector object that represents the difference between this point and another point
+     */
     public Vector subtract(Point p) {
         return new Vector(xyz.subtract(p.xyz));
     }
 
-    public Point add(Point p) {
-        return new Point(xyz.add(p.xyz));
+    /**
+     * Returns a new Point object that represents the sum of this point and a vector.
+     * Uses the add function in Double3
+     *
+     * @param v the vector to add to this point
+     * @return a new Point object that represents the sum of this point and a vector
+     */
+    public Point add(Vector v) {
+        return new Point(xyz.add(v.xyz));
     }
 
+    /**
+     * Returns the square of the distance between this point and another point.
+     *
+     * @param p the other point to calculate the distance to
+     * @return the square of the distance between this point and another point
+     */
     public double distanceSquared(Point p) {
         return xyz.d1 * p.xyz.d1 + xyz.d2 * p.xyz.d2 + xyz.d3 * p.xyz.d3;
     }
 
+    /**
+     * Returns the distance between this point and another point by using distanceSquared function.
+     *
+     * @param p the other point to calculate the distance to
+     * @return the distance between this point and another point
+     */
     public double distance(Point p) {
         return Math.sqrt(distanceSquared(p));
     }
@@ -46,6 +90,4 @@ public class Point {
         }
         return false;
     }
-
-
 }
