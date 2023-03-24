574f079f4a7b54c8f278fc84ab8f187e3e8cf1a7
dants <danieltsirkin2@gmail.com>
2023-03-24 06:31:38 +0300

    first commit

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -1,21 +1,44 @@
 package primitives;
 
+/**
+ * The Ray class represents a ray in 3D space. A ray is defined by a starting point and a direction.
+ */
 public class Ray {
+
     final private Point p0;
+
     final private Vector dir;
 
+    /**
+     * Constructs a new Ray object with the given starting point and direction.
+     *
+     * @param p The starting point of the ray.
+     * @param v The direction of the ray.
+     */
     public Ray(Point p, Vector v) {
         p0 = p;
         dir = v.normalize();
     }
 
+    /**
+     * Returns the starting point of the ray.
+     *
+     * @return The starting point of the ray.
+     */
     public Point getP0() {
         return p0;
     }
 
+    /**
+     * Returns the direction of the ray.
+     *
+     * @return The direction of the ray.
+     */
     public Vector getDir() {
         return dir;
     }
+
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -26,7 +49,8 @@ public class Ray {
         return false;
     }
 
-    @Override
+
+        @Override
     public String toString() {
         return "Ray{" +
                 "p0=" + p0.toString() +
