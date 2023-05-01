15d5eb2732bc22917e2ceb3ffb3ead28230341ad
dants <danieltsirkin2@gmail.com>
2023-05-01 18:28:50 +0300

    progress with the project

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -5,26 +5,58 @@ import primitives.Ray;
 
 import java.util.*;
 
-public class Geometries implements Intersectable{
-    List <Intersectable> geometries;
-    public Geometries(){
+/**
+ * The Geometries class represents a collection of intersectable objects.
+ * It implements the Intersectable interface and provides methods to add new geometries to the collection
+ * and find intersections of a given Ray with all the geometries in the collection.
+ */
+public class Geometries implements Intersectable {
+
+    /**
+     * A list that holds all the geometries in the collection.
+     */
+    List<Intersectable> geometries;
+
+    /**
+     * Constructs a new empty Geometries collection.
+     */
+    public Geometries() {
         geometries = new LinkedList();
     }
-    public Geometries(Intersectable... geometries){
-        this.geometries= List.of(geometries);
+
+    /**
+     * Constructs a new Geometries collection from an array of Intersectable objects.
+     * @param geometries an array of Intersectable objects to add to the collection.
+     */
+    public Geometries(Intersectable... geometries) {
+        this.geometries = List.of(geometries);
     }
-    public void add(Intersectable... geometries){
+
+    /**
+     * Adds Intersectable objects to the collection.
+     * @param geometries an array of Intersectable objects to add to the collection.
+     */
+    public void add(Intersectable... geometries) {
         this.geometries.addAll(List.of(geometries));
     }
-    public List<Point> findIntersections(Ray ray){
-        boolean isIntersect=false;
-        for(Intersectable i:geometries){
-            if(i.findIntersections(ray)!=null)isIntersect=true;
+
+    @Override
+    public List<Point> findIntersections(Ray ray) {
+        boolean isIntersect = false;
+        for (Intersectable i : geometries) {
+            if (i.findIntersections(ray) != null) {
+                isIntersect = true;
+            }
         }
-        if(!isIntersect)return null;
-        List <Point> intersects = new LinkedList();
-        for(Intersectable i:geometries){
-            if(i.findIntersections(ray)!=null)intersects.addAll(i.findIntersections(ray));
+        if (!isIntersect) {
+            return null;
+        }
+        List<Point> intersects = new LinkedList();
+        for (Intersectable i : geometries) {
+            List<Point> geometryIntersects = i.findIntersections(ray);
+            if (geometryIntersects != null) {
+                intersects.addAll(geometryIntersects);
+            }
         }
         return intersects;
     }
--- src/geometries/Intersectable.java
+++ src/geometries/Intersectable.java
@@ -3,7 +3,14 @@ package geometries;
 import primitives.*;
 
 import java.util.List;
-
+/**
+ * The abstract class Intersectable represents intersectable objects.
+ */
 public interface Intersectable {
+    /**
+     *Searches for intersections between a Ray and geometries.
+     *@param ray a Ray object representing the ray to check for intersections
+     *@return a List object of Point type representing the intersections found
+     **/
     public List<Point> findIntersections(Ray ray);
 }
